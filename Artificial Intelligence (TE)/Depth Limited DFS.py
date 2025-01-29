7efc4070eef1ab374ab303c192b0c01ac8a9db10
Craig <craigdagama@gmail.com>
2025-01-29 14:44:26 +0530

    Create Depth Limited DFS.py

---
+++ Artificial Intelligence (TE)/Depth Limited DFS.py
@@ -0,0 +1,31 @@
+# Depth Limited DFS
+
+tree = {'A':['B','C'], 'B':['D','E'], 'C':['F','G'], 'D':[], 'E':[], 'F':[], 'G':[]}
+
+start = input("Enter the start state: ").strip().upper()
+goal = input("Enter the goal state: ").strip().upper()
+depth_limit = int(input("Enter the depth limit: "))
+path = []
+level = 0
+def dldfs (start,goal,tree,level,path,depth_limit):
+    print("Current level is",level)
+    path.append(start)
+    if start == goal:
+        print("Search ends")
+        return path
+    if level == depth_limit:
+        return False
+    print("Expanding current node",start)
+    neighbour = tree[start]
+    for i in neighbour:
+        if dldfs(i,goal,tree,level+1,path,depth_limit):
+            return True
+            path.pop()
+    return False
+result = dldfs(start,goal,tree,level,path,depth_limit)
+if (result):
+    print("Goal Node Found")
+    print("Path: ",path)
+else:
+    print("Goal Node Not Found")
+        
